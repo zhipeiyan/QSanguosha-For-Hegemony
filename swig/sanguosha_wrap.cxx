84ebb140517673f8315336b40008b6cea87ea630
Zhipei Yan <zhipei.y.yan@gmail.com>
2023-03-08 03:44:29 -0600

    Fix bool to QString in GNU C++ compiler

--- swig/sanguosha_wrap.cxx
+++ swig/sanguosha_wrap.cxx
@@ -4302,7 +4302,7 @@ QString LuaViewAsSkill::getExpandPile() const
     int error = lua_pcall(L, 1, 1, 0);
     if (error) {
         Error(L);
-        return false;
+        return QString::number(false);
     } else {
         const char *result = lua_tostring(L, -1);
         lua_pop(L, 1);
